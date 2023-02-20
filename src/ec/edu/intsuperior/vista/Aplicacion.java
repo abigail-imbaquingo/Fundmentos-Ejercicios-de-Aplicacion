75264c5a3f01449b5f51e941955c4f5113571a05
HP <HP@LAPTOP-JL6FOH98>
2023-02-20 18:36:59 -0500

    Primera version de ejercicios de fudamentos de programacion

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,95 @@
+package ec.edu.intsuperior.vista;
+
+import java.util.Scanner;
+
+public class Aplicacion {
+    
+    public static void main(String[] args) {
+        desintegrarNumero();
+        }
+public static void promedios(){
+        Scanner leer= new Scanner(System.in);
+        double n,acu=0,prom;
+        for(int i=1;i<=5;i++){
+            System.out.println("Ingrese nota "+i+":");
+            n=leer.nextDouble();
+            acu=acu+n;
+        }
+        prom=acu/5;
+System.out.println("El promedio acumulado es: " +prom);
+if (prom<=2){
+    System.out.println("Pierde la matricula");
+} else {
+    if (prom<=4){
+        System.out.println("Remedial");
+    }else{
+        if (prom<=6){
+            System.out.println("Supletorio");
+        }else{
+            if(prom<=7){
+                System.out.println("Vuelve a dar el exámen");
+            }else{
+                System.out.println("Aprobado");
+            }
+        }
+    }
+  }
+}
+
+public static void factorial(){
+       Scanner leer= new Scanner(System.in);
+       int num,fac=1;
+        System.out.println("Ingresa el numero a ser calculado");
+       num=leer.nextInt();
+       for(int i=1;i<=num;i++){
+           fac=fac*i;   
+       }
+        System.out.println("el factorial de: "+num+" es:"+fac);
+    }
+    
+public static void sumaDosNumeros(){
+        Scanner leer = new Scanner(System.in);
+        int n1,n2;
+        System.out.println("Ingresa el numero 1:");
+        n1=leer.nextInt();
+        System.out.println("Ingresa el numero 2:");
+        n2=leer.nextInt();
+        System.out.println("La suma es: "+(n1+n2));
+    }
+
+public static void generarNumerosEnteros(){
+        Scanner leer= new Scanner(System.in);
+        int n1, n2;
+        System.out.println("Ingresa el limite inferior");
+        n1=leer.nextInt();
+        System.out.println("Ingresa el numero superior mayor a:"+n1);
+        n2=leer.nextInt();
+        for(int i=n1;i<(n2-1);i++){
+            System.out.print(i+1+" ");
+        }
+    }
+
+public static void desintegrarNumero(){
+        Scanner leer = new Scanner(System.in);
+        int num,aux=0;
+        System.out.println("Ingresa un numero positivo");
+        num= leer.nextInt();
+       //aux=numero-numero/10*10;
+       while(num>aux){
+        aux=num%10;
+        num=num/10;
+        System.out.println(aux+"    "+num);
+       }
+    }
+
+public static int retornarEdad(){
+       Scanner leer= new Scanner(System.in);
+        int x, ac=2023,edad;
+        float y;
+        double z;
+        System.out.println("Ingresa el año de nacimiento");
+        x=leer.nextInt();
+        edad=ac-x;
+        return edad;
+    }   
+}
